df7a38b902556e176b03a5cae928b8cd72271a55
Dmitry Plevkov <wwarne@gmail.com>
2017-01-25 18:21:15 +0300

    pretty print first try

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,24 @@
 import json
+import sys
 
 
 def load_data(filepath):
-    pass
+    try:
+        with open(filepath, encoding='utf-8') as f:
+            return json.load(f)
+    except OSError:
+        print('Can\'t open the file `{}`'.format(filepath))
+    except json.JSONDecodeError:
+        print('Can\'t decode content of the file `{}`. Please check the file').format(filepath)
+    sys.exit(1)
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, ensure_ascii=False, indent=4, sort_keys=True))
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) == 1:
+        print('Usage: python pprint_json.py <path to file>')
+        sys.exit(1)
+    pretty_print_json(load_data(sys.argv[1]))
